1b305b309108aeb6a6cd6ba00c430c0b9378f853
mareksuma1985 <msuma@wp.pl>
2019-04-11 23:32:06 +0200

    minor changes

--- PhoneUAV/src/main/java/pl.bezzalogowe.PhoneUAV/Autopilot.java
+++ PhoneUAV/src/main/java/pl.bezzalogowe.PhoneUAV/Autopilot.java
@@ -1,84 +1,174 @@
 package pl.bezzalogowe.PhoneUAV;
 
-import java.util.concurrent.ScheduledExecutorService;
+import android.location.LocationManager;
+import com.stormbots.MiniPID;
 
-/**
- * Created by uzytkownik on 2018-03-10.
- * This is a stub. When "autopilot" is enabled, moving elevator sets target pitch between -30 and 30 degrees.
- */
+/** https://github.com/tekdemo/MiniPID-Java */
 
 public class Autopilot {
+    public MiniPID pid;
     MainActivity main;
-
     boolean stabilize_roll = false;
     boolean stabilize_pitch = false;
-
+    boolean auto_yaw = false;
+    int period = 100;
     double target_roll = 0;
     double target_pitch = 0;
+    double target_altitude;
+    boolean fakeAltitude = false;
+    Thread altitudeHoldThread;
+    double proportional = 0;
+    double integral = 0;
+    double derivative = 0;
 
-    ScheduledExecutorService executorAutopilot;
-
-    public void startAutopilot(MainActivity activityArgument) {
-        main = activityArgument;
-    }
-
-    private void stablilizeRoll() {
-        /** Stabilizes roll only */
-
-        main.ail = (int) (-main.gravityObject.angle_roll / 0.18) + 1500;
-
-        if (main.outputMode == main.USC16) {
-            main.ch340commObject.SetPosition(main.outputsAileron, main.ail, (byte) 100);
-
-            main.ch340commObject.SetPosition(main.outputsFlaperonLeft, main.ail, (byte) 100);
-            main.ch340commObject.SetPosition(main.outputsFlaperonRight, -main.ail + 3000, (byte) 100);
-        }
-
-        main.seekbarAIL.setProgress(main.inputObject.scaleDown(main.ail));
-    }
-
-    private void stablilizePitch() {
-        /** Stabilizes pitch only */
-
-        if (main.gravityObject.angle_pitch > 90) {
-            // inverted and climbing
-            main.ele = 2500;
-        } else if (main.gravityObject.angle_pitch < -90) {
-            // inverted and descending
-            main.ele = 500;
-        } else {
-            main.ele = (int) ((main.gravityObject.angle_pitch - target_pitch) / 0.09 + 1500);
-        }
-
-        if (main.outputMode == main.USC16) {
-            main.ch340commObject.SetPosition(main.outputsElevator, main.ele, (byte) 100);
-        }
-
-        main.seekbarELEV.setProgress(main.inputObject.scaleDown(main.ele));
-    }
-
-    public void processGravity() {
-        if (stabilize_pitch == true && stabilize_roll == true) {
-            /** Stabilizes both pitch and roll */
-            stablilizePitch();
-            stablilizeRoll();
-        } else if (stabilize_pitch == true) {
-            stablilizePitch();
-        } else if (stabilize_roll == true) {
-            stablilizeRoll();
-        }
-    }
-
-    public void disableAutopilot(MainActivity argActivity) {
+    public void startAutopilot(MainActivity argActivity) {
         main = argActivity;
+        stabilize_pitch = true;
+        stabilize_roll = true;
+        altitudeHoldThread = new Thread(new AutopilotThread());
+        altitudeHoldThread.start();
+    }
+
+    public void stopAutopilot(MainActivity argActivity) {
+        main = argActivity;
+        stabilize_pitch = false;
+        stabilize_roll = false;
         try {
-            executorAutopilot.shutdownNow();
-            if (executorAutopilot.isShutdown()) {
-                main.update.updateConversationHandler.post(new updateTextThread(main.text_server, "Autopilot stopped"));
-            }
+            altitudeHoldThread.interrupt();
+            altitudeHoldThread = null;
+            main.update.updateConversationHandler.post(new updateTextThread(main.text_server, "Autopilot stopped"));
         } catch (Exception e) {
+            e.printStackTrace();
             main.update.updateConversationHandler.post(new updateTextThread(main.text_server, "Autopilot NOT stopped"));
         }
-        executorAutopilot = null;
+    }
+
+    public void startFollowingWaypoints(MainActivity argActivity) {
+        main = argActivity;
+        if (main.locObject.waypointNext != null) {
+            main.autopilot.auto_yaw = true;
+        } else {
+            main.locObject.waypointNext = new android.location.Location(LocationManager.GPS_PROVIDER);
+            int size = main.locObject.nextWaypoint();
+            if (size == 0) {
+                main.locObject.waypointNext = null;
+                /*
+                main.locObject.waypointNext.setLatitude(0);
+                main.locObject.waypointNext.setLongitude(0);
+                main.locObject.waypointNext.setAltitude(300);
+                */
+            }
+        }
+    }
+
+    public void stopFollowingWaypoints(MainActivity argActivity) {
+        main = argActivity;
+        main.autopilot.auto_yaw = false;
+    }
+
+    class AutopilotThread implements Runnable {
+        @SuppressWarnings("all")
+        public void run() {
+
+            /** sets target altitude to the barometric altitude at the moment of enabling autopilot */
+            main.autopilot.target_altitude = main.pressureObject.altitudeBarometric;
+
+            pid = new MiniPID(proportional, integral, derivative);
+            /** Now loop forever, waiting to receive packets and printing them. */
+            while (altitudeHoldThread != null) {
+
+                target_pitch = pid.getOutput((double) main.pressureObject.altitudeBarometric, target_altitude);
+
+                short aileronrvalue = 0;
+                short elevatorvalue = 0;
+                short ruddervalue = 0;
+
+                /** PID target pitch hold */
+                if (stabilize_pitch && main.inputObject.controllerY2value == 0) {
+                    String pString = String.format("%.2f", proportional);
+                    String iString = String.format("%.2f", integral);
+                    String dString = String.format("%.2f", derivative);
+                    String outputString = String.format("%.2f", target_pitch);
+
+                    main.update.updateConversationHandler.post(new updateTextThread(main.text_server, "P= " + pString + "\tI= " + iString + "\tD= " + dString + "\ttarget_pitch= " + outputString + "\u00B0"));
+
+                    /** P-- elevator adjustment */
+                    elevatorvalue = (short) ((target_pitch - main.gravityObject.angle_pitch) * 364);
+                    /* target > actual => elevatorvalue > 0
+                     * target < actual => elevatorvalue < 0 */
+
+                    if (elevatorvalue > 32767) {
+                        elevatorvalue = 32767;
+                    }
+                    if (elevatorvalue < -32767) {
+                        elevatorvalue = -32767;
+                    }
+
+                    if (main.outputMode == main.USC16) {
+                        main.ch340commObject.SetPositionPrecisely(main.outputsElevator, -elevatorvalue, period, main.ch340commObject.servoElevatorMin, main.ch340commObject.servoElevatorMax);
+                    }
+
+                    main.ele = (int) (-elevatorvalue / 32.767 + 1500);
+
+                    main.update.updateConversationHandler.post(new updateProgressThread(main.seekbarELEV, main.inputObject.scaleDown(main.ele)));
+                    main.update.updateConversationHandler.post(new updateTextThread(main.dutyCycleELEV, Integer.toString(main.inputObject.scaleDown(main.ele)) + "%"));
+                }
+
+                /** P-- roll adjustment */
+                if (stabilize_roll && main.inputObject.controllerX2value == 0) {
+                    // -32760, 32760
+                    double angle_roll_clipped;
+                    if (main.gravityObject.angle_roll > 90) {
+                        angle_roll_clipped = 90;
+                    } else if (main.gravityObject.angle_roll < -90) {
+                        angle_roll_clipped = -90;
+                    } else {
+                        angle_roll_clipped = main.gravityObject.angle_roll;
+                    }
+                    
+                    aileronrvalue = (short) (angle_roll_clipped * 364);
+                    main.ail = (int) ((-aileronrvalue / 32.767) + 1500);
+
+                    if (main.outputMode == main.USC16) {
+                        main.ch340commObject.SetPositionPrecisely(main.outputsAileron, aileronrvalue, period, 500, 2500);
+
+                        //TODO flaps
+                        main.ch340commObject.SetPosition(main.outputsFlaperonLeft, main.ail, period, 500, 2500);
+                        main.ch340commObject.SetPosition(main.outputsFlaperonRight, -main.ail + 3000, period, 500, 2500);
+                    }
+
+                    main.update.updateConversationHandler.post(new updateProgressThread(main.seekbarAIL, main.inputObject.scaleDown(main.ail)));
+                    main.update.updateConversationHandler.post(new updateTextThread(main.dutyCycleAIL, Integer.toString(main.inputObject.scaleDown(main.ail)) + "%"));
+                }
+
+                /** P-- yaw (rudder) adjustment */
+                if (auto_yaw && main.inputObject.controllerX1value == 0 && main.locObject.waypointNext != null) {
+                    double difference = (((360 + (main.magObject.azimuth - main.magObject.heading)) % 360) - 180);
+
+                    main.update.updateConversationHandler.post(new updateTextThread(main.angle_text_heading, (int) main.magObject.heading + "\u00B0\n" + (int) main.magObject.azimuth + "\u00B0\n" + (int) difference + "\u00B0"));
+
+                    ruddervalue = (short) (difference * 182);
+
+                    if (main.outputMode == main.USC16) {
+                        main.ch340commObject.SetPositionPrecisely(main.outputsRudder, ruddervalue, 200 /*headingPeriod*/, main.ch340commObject.servoRudderMin, main.ch340commObject.servoRudderMax);
+                    }
+
+                    main.rdr = (int) ((-ruddervalue / 32.767) + 1500);
+                    main.update.updateConversationHandler.post(new updateProgressThread(main.seekbarRDR, main.inputObject.scaleDown(main.rdr)));
+                    main.update.updateConversationHandler.post(new updateTextThread(main.dutyCycleRDR, Integer.toString(main.inputObject.scaleDown(main.rdr)) + "%"));
+                }
+
+                /** flying wing */
+                if (main.outputMode == main.USC16) {
+                    main.main.inputObject.mixElevons(aileronrvalue, elevatorvalue);
+                }
+
+                try {
+                    Thread.sleep(period);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
