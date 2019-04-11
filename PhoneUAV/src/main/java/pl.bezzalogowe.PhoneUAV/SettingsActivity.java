4c3d52f9a8fe2d5efa5633daf8915725d9a6e655
mareksuma1985 <msuma@wp.pl>
2019-04-11 23:47:09 +0200

    minor changes

--- PhoneUAV/src/main/java/pl.bezzalogowe.PhoneUAV/SettingsActivity.java
+++ PhoneUAV/src/main/java/pl.bezzalogowe.PhoneUAV/SettingsActivity.java
@@ -19,22 +19,23 @@ import android.preference.RingtonePreference;
 import android.support.v4.app.NavUtils;
 import android.text.TextUtils;
 import android.view.MenuItem;
+
 import java.util.List;
 
 /**
-<p>
-A {@link PreferenceActivity} that presents a set of application settings.
-On handset devices, settings are presented as a single list. On tablets,
-settings are split by category, with category headers shown to the left of
-the list of settings.
-<p/>
-<p>
-See <a href="http://developer.android.com/design/patterns/settings.html">
-Android Design: Settings</a> for design guidelines and the
-<a href="http://developer.android.com/guide/topics/ui/settings.html">Settings API Guide</a>
-for more information on developing a Settings UI.
-</p>
-*/
+ * <p>
+ * A {@link PreferenceActivity} that presents a set of application settings.
+ * On handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ * <p/>
+ * <p>
+ * See <a href="http://developer.android.com/design/patterns/settings.html">
+ * Android Design: Settings</a> for design guidelines and the
+ * <a href="http://developer.android.com/guide/topics/ui/settings.html">Settings API Guide</a>
+ * for more information on developing a Settings UI.
+ * </p>
+ */
 
 public class SettingsActivity extends PreferenceActivity {
     /**
@@ -186,6 +187,16 @@ public class SettingsActivity extends PreferenceActivity {
             bindPreferenceSummaryToValue(findPreference("outputs-rudder"));
             bindPreferenceSummaryToValue(findPreference("outputs-elevon-left"));
             bindPreferenceSummaryToValue(findPreference("outputs-elevon-right"));
+
+            bindPreferenceSummaryToValue(findPreference("servo-elevator-min"));
+            bindPreferenceSummaryToValue(findPreference("servo-elevator-max"));
+
+            bindPreferenceSummaryToValue(findPreference("servo-rudder-min"));
+            bindPreferenceSummaryToValue(findPreference("servo-rudder-max"));
+
+            bindPreferenceSummaryToValue(findPreference("servo-elevon-min"));
+            bindPreferenceSummaryToValue(findPreference("servo-elevon-max"));
+
             bindPreferenceSummaryToValue(findPreference("throttle-min"));
             bindPreferenceSummaryToValue(findPreference("throttle-max"));
         }
@@ -236,6 +247,7 @@ public class SettingsActivity extends PreferenceActivity {
             bindPreferenceSummaryToValue(findPreference("protocol"));
             bindPreferenceSummaryToValue(findPreference("server-port"));
             bindPreferenceSummaryToValue(findPreference("server-path"));
+            bindPreferenceSummaryToValue(findPreference("email-list"));
         }
 
         @Override
@@ -249,4 +261,3 @@ public class SettingsActivity extends PreferenceActivity {
         }
     }
 }
-
