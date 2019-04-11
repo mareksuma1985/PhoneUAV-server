113456bcecc0f4d55372734f35f82b767afdadcb
mareksuma1985 <msuma@wp.pl>
2019-04-11 23:33:45 +0200

    minor changes

--- PhoneUAV/src/main/java/pl.bezzalogowe.PhoneUAV/Barometer.java
+++ PhoneUAV/src/main/java/pl.bezzalogowe.PhoneUAV/Barometer.java
@@ -5,19 +5,29 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.util.Log;
+import java.util.Locale;
 
 public class Barometer {
-    /**
-     * https://www.daftlogic.com/sandbox-google-maps-find-altitude.htm
-     * https://www.youtube.com/watch?v=SyGxDEjXYIU
-     */
+    public float pressure_mean_sealevel;
+    // needed only for calibration at startup
+    public float pressureBarometricRecent;
+
+/**
+     https://www.daftlogic.com/sandbox-google-maps-find-altitude.htm
+     https://www.youtube.com/watch?v=SyGxDEjXYIU
+*/
 
     MainActivity main;
     SensorManager snsMgr;
     Sensor barometer;
-    float pressure_mean_sealevel;
-    //pressure_mean_sealevel = SensorManager.PRESSURE_STANDARD_ATMOSPHERE;
     float altitudeBarometric = 0;
+    float altitudeBarometricRecent = 0;
+
+    /** Takes GPS altitude and measured barometric pressure, returns pressure at sea level */
+    public static float getSealevelPressure(float alt, float p) {
+        float sealevel = (float) (p / Math.pow(1 - (alt / 44330.0f), 5.255f));
+        return sealevel;
+    }
 
     public void startBarometer(MainActivity argActivity) {
         main = argActivity;
@@ -28,16 +38,27 @@ public class Barometer {
 
     public void calculateAltitude(SensorEvent event) {
         float[] values = event.values;
+        pressureBarometricRecent = values[0];
         altitudeBarometric = SensorManager.getAltitude(pressure_mean_sealevel, values[0]);
 
-        double altitudeGPS = 0;
-        if (main.locObject.recentLocation != null) {
-            altitudeGPS = main.locObject.recentLocation.getAltitude();
+        if (main.autopilot.fakeAltitude && main.seekbarTEST != null) {
+            /** for testing, takes value from seek bar */
+            //main.locObject.recentLocation.setAltitude(100 + main.seekbarTEST.getProgress());
+            altitudeBarometricRecent = 100 + main.seekbarTEST.getProgress();
+        } else {
+            /** sends altitude feedback and updates recent value only if difference is greater than 0.5m */
+            if (altitudeBarometric < altitudeBarometricRecent - 0.5 || altitudeBarometric > altitudeBarometricRecent + 0.5) {
+                Thread feedbackBarometer = new Thread(new Wrap());
+                feedbackBarometer.start();
+                altitudeBarometricRecent = altitudeBarometric;
+            }
         }
-        main.update.updateConversationHandler.post(new updateTextThread(main.altitude_text, altitudeBarometric + "\n(" + altitudeGPS + ")"));
 
-        Thread feedback = new Thread(new Wrap());
-        feedback.start();
+        if (main.locObject.recentLocation != null) {
+            main.update.updateConversationHandler.post(new updateTextThread(main.altitude_text, String.format(Locale.getDefault(), "%.2f", altitudeBarometricRecent) + " m\n(" + main.locObject.recentLocation.getAltitude() + " m)"));
+        } else {
+            main.update.updateConversationHandler.post(new updateTextThread(main.altitude_text, String.format(Locale.getDefault(), "%.2f", altitudeBarometricRecent) + " m\n"));
+        }
     }
 
     class Wrap implements Runnable {
